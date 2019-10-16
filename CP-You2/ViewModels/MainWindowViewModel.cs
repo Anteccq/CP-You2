2175d56aa7554c3cbe72a2d0e8f521c86f140964
AntiqueR <39259186+AntiquePendulam@users.noreply.github.com>
2019-10-16 22:16:08 +0900

    add Display TotalPercentage

---
+++ CP-You2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CP_You2.Models;
+using Reactive.Bindings;
+using Prism.Mvvm;
+
+namespace CP_You2.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public ReactiveProperty<CpuPercentageManager> CpuPercentage { get; }
+
+        public MainWindowViewModel()
+        {
+            CpuPercentage = new ReactiveProperty<CpuPercentageManager>(new CpuPercentageManager());
+        }
+    }
+}

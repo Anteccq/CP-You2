faea0dc8e40f1aa0306bec2376bf5400c2094e60
AntiqueR <39259186+AntiquePendulam@users.noreply.github.com>
2019-10-22 23:38:58 +0900

    add : Viewの表示切替を追加

---
+++ CP-You2/Models/DisplayModeManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Prism.Mvvm;
+
+namespace CP_You2.Models
+{
+    public class DisplayModeManager : BindableBase
+    {
+        private bool _isTotalMode = true;
+
+        public bool IsTotalMode
+        {
+            get => _isTotalMode;
+            set => this.SetProperty(ref _isTotalMode, value);
+        }
+
+        public void ModeChange()
+        {
+            IsTotalMode = !IsTotalMode;
+        }
+    }
+}

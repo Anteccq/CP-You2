7dae476077e506fea60f17e744cf60248fa6c2d7
AntiqueR <39259186+AntiquePendulam@users.noreply.github.com>
2019-10-25 21:24:15 +0900

    add : Windowの位置をReadWriteする処理を追加

--- CP-You2/Models/WindowManager.cs
+++ CP-You2/Models/WindowManager.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using CP_You2.Interfaces;
 using Prism.Mvvm;
+using Utf8Json;
 
 namespace CP_You2.Models
 {
-    public class WindowManager : BindableBase
+    public class WindowManager : BindableBase, IDataService
     {
+        private static readonly string DIRECTORY_PATH =
+            $@"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\ProjectATR\CP-You2";
+
+        private const string FILE_NAME = @"\Window.config";
+
+        private const string ERR_LOG_FILE = @"./Error.log";
+
         private bool _isTotalMode = true;
 
         public bool IsTotalMode
@@ -16,9 +27,75 @@ namespace CP_You2.Models
             set => this.SetProperty(ref _isTotalMode, value);
         }
 
+        private double _left;
+
+        public double Left
+        {
+            get => _left;
+            set
+            {
+                if (SystemParameters.VirtualScreenWidth < value) value = SystemParameters.VirtualScreenWidth;
+                else if (0 > value) value = 0;
+                this.SetProperty(ref _left, value);
+            }
+        }
+
+        private double _top;
+
+        public double Top
+        {
+            get => _top;
+            set
+            {
+                if (SystemParameters.VirtualScreenHeight < value) value = SystemParameters.VirtualScreenHeight;
+                else if (0 > value) value = 0;
+                this.SetProperty(ref _top, value);
+            }
+        }
+
+        public WindowManager()
+        {
+            this.PropertyChanged += async (a, e) => await this.SaveAsync();
+        }
+
         public void ModeChange()
         {
             IsTotalMode = !IsTotalMode;
+        }
+
+        public async Task SaveAsync()
+        {
+            if (!Directory.Exists(DIRECTORY_PATH)) Directory.CreateDirectory(DIRECTORY_PATH);
+            try
+            {
+                await using var stream = File.OpenWrite(DIRECTORY_PATH + FILE_NAME);
+                await JsonSerializer.SerializeAsync(stream, this);
+            }
+            catch (Exception e)
+            {
+                await using var stream = new StreamWriter(ERR_LOG_FILE, true, Encoding.UTF8);
+                await stream.WriteLineAsync($"{DateTime.Now} : {e.Message}");
+            }
+
+        }
+
+        public async Task LoadAsync()
+        {
+            if (!Directory.Exists(DIRECTORY_PATH) || !File.Exists(DIRECTORY_PATH + FILE_NAME)) return;
+
+            try
+            {
+                await using var stream = File.OpenRead(DIRECTORY_PATH + FILE_NAME);
+                var windowData = await JsonSerializer.DeserializeAsync<WindowManager>(stream);
+                this.Left = windowData.Left;
+                this.Top = windowData.Top;
+                this.IsTotalMode = windowData.IsTotalMode;
+            }
+            catch (Exception e)
+            {
+                await using var stream = new StreamWriter(ERR_LOG_FILE, true, Encoding.UTF8);
+                await stream.WriteLineAsync($"{DateTime.Now} : {e.Message}");
+            }
         }
     }
 }
--- CP-You2/ViewModels/MainWindowViewModel.cs
+++ CP-You2/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using CP_You2.Models;
 using Reactive.Bindings;
 using Prism.Mvvm;
@@ -15,11 +16,17 @@ namespace CP_You2.ViewModels
 
         public ReactiveCommand ModeChangeCommand { get; }
 
+        public ReactiveCommand WindowDataLoadAsyncCommand { get; }
+
         public MainWindowViewModel()
         {
             WindowManager = new WindowManager();
-            ModeChangeCommand = new ReactiveCommand(); 
+            ModeChangeCommand = new ReactiveCommand();
             ModeChangeCommand.Subscribe(_ => WindowManager.ModeChange());
+            WindowDataLoadAsyncCommand = new ReactiveCommand();
+            WindowDataLoadAsyncCommand.Subscribe(async _ => await WindowManager.LoadAsync());
         }
+
+        public async Task WindowLoadAsync() => await WindowManager.LoadAsync();
     }
 }

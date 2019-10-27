a0c24e137187c733ab2e32ef5ef0f9b24ee4f614
AntiqueR <39259186+AntiquePendulam@users.noreply.github.com>
2019-10-27 19:44:09 +0900

    add window correcting

---
+++ CP-You2/Models/WindowData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Mvvm;
+
+namespace CP_You2.Models
+{
+    public class WindowData : BindableBase
+    {
+        private bool _isTotalMode = true;
+
+        public bool IsTotalMode
+        {
+            get => _isTotalMode;
+            set => this.SetProperty(ref _isTotalMode, value);
+        }
+
+        private double _left;
+
+        public double Left
+        {
+            get => _left;
+            set => this.SetProperty(ref _left, value);
+        }
+
+        private double _top;
+
+        public double Top
+        {
+            get => _top;
+            set => this.SetProperty(ref _top, value);
+        }
+    }
+}
